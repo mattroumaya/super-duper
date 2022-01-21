269e374fc3998de767faee0f573aef4236775de6
Matt Roumaya <matthewroumaya@gmail.com>
2022-01-21 10:34:49 -0500

    update and comment python script

--- get data.py
+++ get data.py
@@ -2,30 +2,32 @@
 # coding: utf-8
 
 #%%
-
-import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-import time
-import camelot
-import ghostscript
-import os
-import string
-from camelot.handlers import PDFHandler
-import shutil
-from webdriver_manager.chrome import ChromeDriverManager
-
-
 #%%
-# Set up options so PDFs download automatically
 
+
+
+# ! ! ! ! ! ! ! ! 
+# ! update these parameters !
+
+# same as setwd() in R - location where pdfs will be saved
+download_dir = "C:\\me\path\something\pdfs"
+
+# the location of your chromedriver .exe file
+path_to_chromedriver = "C:\me\path\chromedriver\chromedriver.exe"
+
+
+# ! ! ! ! ! ! ! ! 
+
+
+
+
+# set up options so pdfs download automatically
+# this sets options in chrome so that pdfs are automatically downloaded without 
+# prompting user to click a "download" button
 chrome_options = Options()
-
-download_dir = "C:\\Matt\Python\ACGME\pdfs"
-
 chrome_options.add_experimental_option('prefs',  {
     "download.default_directory": download_dir,
     "download.prompt_for_download": False,
@@ -34,21 +36,26 @@ chrome_options.add_experimental_option('prefs',  {
     }
 )
 
-b = webdriver.Chrome("C:\Matt\Python\chromedriver\chromedriver.exe", options = chrome_options)
+# this script requires chromedriver to be downloaded
+# NOTE chromedriver is regularly updated to keep up with Chrome updates
+# so you may need to update your version of chromedriver often
+# NOTE it is also important that the version of chromedriver matches the version of
+# Chrome that you are using.
+# to download: https://chromedriver.chromium.org/downloads
+b = webdriver.Chrome(path_to_chromedriver, options = chrome_options)
 
+# this opens the web page
 b.get('https://apps.acgme.org/ads/Public/Reports/Report/1')
 
+# this searches the css for #SpecialtyCode, which will map to every drop-down element
 inputs = Select(b.find_element_by_css_selector('#SpecialtyCode'))
-
 input1 = len(inputs.options)
-
-
 options = inputs.options
 
 
 #%%
-# This is the fun part where ~165 pdfs get automatically downloaded
 
+# this is the fun part where ~165 pdfs get automatically downloaded
 names = []
 for items in range(input1):
      inputs.select_by_index(items)
